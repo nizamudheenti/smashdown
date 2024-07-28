9e89322c613fd931b15185e1a9ec125ad321d4ca
NIZAMUDHEEN_T_I <33263597+nizamudheenti@users.noreply.github.com>
2024-07-28 19:48:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,16 @@
 import streamlit as st
 
 st.title("Smashdown 2024")
-page_bg_img = '''
+# Set the background image
+background_image = """
 <style>
-body {
-background-image: url("https://img.freepik.com/free-photo/badminton-concept-with-racket-shuttlecock_23-2149940923.jpg");
-background-size: cover;
+[data-testid="stAppViewContainer"] > .main {
+    background-image: url("https://img.freepik.com/free-photo/badminton-concept-with-racket-shuttlecock_23-2149940923.jpg");
+    background-size: 100vw 100vh;  # This sets the size to cover 100% of the viewport width and height
+    background-position: center;  
+    background-repeat: no-repeat;
 }
 </style>
-'''
+"""
 
-st.markdown(page_bg_img, unsafe_allow_html=True)
+st.markdown(background_image, unsafe_allow_html=True)
