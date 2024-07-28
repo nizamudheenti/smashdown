762dc286d7f26731d0b669be22231c769bec188d
NIZAMUDHEEN_T_I <33263597+nizamudheenti@users.noreply.github.com>
2024-07-28 21:59:30 +0530

    y

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,10 +10,13 @@ st.title('Badminton Tournament Fixtures')
 file_path = "fixture.csv"
 df = pd.read_csv(file_path)
 
-# Display the data category-wise
+# Extract unique categories for the dropdown
 categories = df['Category'].unique()
 
-for category in categories:
-    st.header(f"{category}")
-    category_df = df[df['Category'] == category]
-    st.table(category_df[['Match Number', 'Names', 'Group', 'Court Number', 'Time']])
+# Create a dropdown for category selection
+selected_category = st.selectbox('Select Category', categories)
+
+# Display the fixtures for the selected category
+st.header(f"{selected_category}")
+category_df = df[df['Category'] == selected_category]
+st.table(category_df[['Match Number', 'Names', 'Group', 'Court Number', 'Time']])
