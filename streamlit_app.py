1742cf2841fe22ec83e0437ac42cdd7b4e5dcd40
NIZAMUDHEEN_T_I <33263597+nizamudheenti@users.noreply.github.com>
2024-07-28 19:49:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ background_image = """
 <style>
 [data-testid="stAppViewContainer"] > .main {
     background-image: url("https://img.freepik.com/free-photo/badminton-concept-with-racket-shuttlecock_23-2149940923.jpg");
+    background-size: 100vw 300vh;  # This sets the size to cover 100% of the viewport width and height
     background-position: center;  
     background-repeat: no-repeat;
 }
