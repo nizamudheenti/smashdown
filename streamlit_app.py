201ab1f4ba821fac2778f1b40a647350b2177f9d
NIZAMUDHEEN_T_I <33263597+nizamudheenti@users.noreply.github.com>
2024-08-02 00:46:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,17 +2,17 @@ import streamlit as st
 import pandas as pd
 
 st.set_page_config(page_title="PAI-SMASHDOWN", layout="wide", page_icon="🏸")
-st.logo('17898105.png', icon_image='csv/17898105.png', link='https://smashdown.streamlit.app/')
+st.logo('17898105.png', icon_image='/csv/17898105.png', link='https://smashdown.streamlit.app/')
 
 # Title of the app
 st.header('SMASHDOWN 2024 - PAI')
 st.divider()
 
 # Load the CSV file
-file_path = "csv/fixture.csv"
+file_path = "/csv/fixture.csv"
 df = pd.read_csv(file_path)
 
-knockout = pd.read_csv("csv/knockout.csv")
+knockout = pd.read_csv("/csv/knockout.csv")
 
 columns = ['Fixture', 'Tournament Rules', 'Results']
 
