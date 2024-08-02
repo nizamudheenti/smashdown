e1cf50eeba3bbcc45dbfd6d61f2f769464eebed1
NIZAMUDHEEN_T_I <33263597+nizamudheenti@users.noreply.github.com>
2024-08-03 00:42:44 +0530

    y

--- csv.py
+++ csv.py
@@ -22,45 +22,44 @@ def save_data(mixed_a,mixed_b,men_a,men_b,men_c,women_a,women_b):
            women_b.to_csv("csv/women_b.csv", index=False))
 
 # Main function to create the Streamlit app
-def main():
-    st.title("Badminton Tournament Points Table")
-    st.divider()
-    # Load data
-    mixed_a,mixed_b,men_a,men_b,men_c,women_a,women_b= load_data()
-    
+st.title("Badminton Tournament Points Table")
+mixed_a,mixed_b,men_a,men_b,men_c,women_a,women_b= load_data()
+tab1,tab2=st.tabs(["Points Table","Fixture"])
+with tab1:
     # Display editable table
     with st.expander("Mixed Doubles"):
         st.divider()
         st.markdown("##### Mixed A")
-        edited_mixed_a = st.data_editor(mixed_a, num_rows="dynamic",use_container_width=True)
+        edited_mixed_a = st.data_editor(mixed_a,use_container_width=True)
         st.divider()
         st.markdown("##### Mixed B")
-        edited_mixed_b = st.data_editor(mixed_b, num_rows="dynamic",use_container_width=True)
+        edited_mixed_b = st.data_editor(mixed_b,use_container_width=True)
         st.divider()
     with st.expander("Men's Doubles"):
         st.divider()
         st.markdown("##### Men A")
-        edited_men_a = st.data_editor(men_a, num_rows="dynamic",use_container_width=True)
+        edited_men_a = st.data_editor(men_a,use_container_width=True)
         st.divider()
         st.markdown("##### Men B")
-        edited_men_b = st.data_editor(men_b, num_rows="dynamic",use_container_width=True)
+        edited_men_b = st.data_editor(men_b,use_container_width=True)
         st.divider()
         st.markdown("##### Men C")
-        edited_men_c = st.data_editor(men_c, num_rows="dynamic",use_container_width=True)
+        edited_men_c = st.data_editor(men_c,use_container_width=True)
         st.divider()
     with st.expander("Women's Doubles"):
         st.divider()
         st.markdown("##### Women A")
-        edited_women_a = st.data_editor(women_a, num_rows="dynamic",use_container_width=True)
+        edited_women_a = st.data_editor(women_a,use_container_width=True)
         st.divider()
         st.markdown("##### Women A")
-        edited_women_b = st.data_editor(women_b, num_rows="dynamic",use_container_width=True)
+        edited_women_b = st.data_editor(women_b,use_container_width=True)
         st.divider()
     # Save button
     if st.button("Save"):
         save_data(edited_mixed_a,edited_mixed_b,edited_men_a,edited_men_b,edited_men_c,edited_women_a,edited_women_b)
+        st.success("Group Points table updated successfully!")
+with tab2:
+    edited_fixture=st.data_editor(pd.read_csv("csv/fixture.csv").drop("Unnamed: 0",axis=1),use_container_width=True,hide_index=True)
+    if st.button("Save",key="123"):
+        edited_fixture.to_csv("csv/fixture.csv")
         st.success("Points table updated successfully!")
-
-# Run the app
-if __name__ == "__main__":
-    main()
